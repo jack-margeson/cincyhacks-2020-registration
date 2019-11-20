12618d0a103ebc51e0740dddefeb971ae55e0e30
Jack Margeson <margeson.jack@gmail.com>
2019-11-19 22:08:50 -0500

    Sendy requests w/o API key, added registration-test

--- registration-test/register.js
+++ registration-test/register.js
@@ -1,32 +1,23 @@
-// Grab Airtable and Postal keys.
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
 const POSTAL_LIST_ID = process.env.POSTAL_LIST_ID;
 
-// Create new airtable base using user API key and base ID.
 var Airtable = require("airtable");
-var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_BASE_ID);
+var base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base("appP0xGQjxYDqFQSk");
 
-// Require statements.
 const express = require("express");
 const request = require("request");
 
-// Init. Express.js server.
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 8080;
 app.use(cors());
 
-// Main page.
 app.get("/", (req, res) => {
   res.send("Welcome to the CincyHacks2020 registration station!");
 });
 
-// Registration page.
 app.get("/register", (req, res) => {
-  // Gets data through an array of params.
   data = req.query.array;
-  // Creates new Airtable entry.
   base("Signups").create(
     [
       {
@@ -51,33 +42,25 @@ app.get("/register", (req, res) => {
     ],
     function(err, records) {
       if (err) {
-        // Push error to console.
         console.error(err);
         return;
       }
       records.forEach(function(record) {
-        // Log new Airtable entry ID.
         console.log(record.getId());
       });
     }
   );
 
-  // Registration confirmation.
   res.send("The registration has been completed.");
 });
 
-// Subscribe page.
 app.get("/subscribe", (req, res) => {
-  // Gets queries.
   data = req.query;
   subscribeUser(data.email, data.name);
-  // Subscribe confirmation.
   res.send("Subscribe request receieved.");
 });
 
-// Subscription function.
 function subscribeUser(email, name) {
-  // Sents request and subscribes passed email and name to Postal ID provided.
   request
     .post("https://postal.hackclub.com/subscribe")
     .form({ list: POSTAL_LIST_ID, email: email, name: name });
